f64d7ede2c7ded04e8388a5f8a187fa75334c846
soukainag <soukaina.guassmi@gmail.com>
2025-04-24 16:22:31 +0100

    enhacement front end

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -5,22 +5,20 @@ import { RouterModule } from "@angular/router";
 import { SplitterModule } from 'primeng/splitter';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
-import { map } from 'rxjs/operators';
 import { CartService } from "././cart/data-access/cart.service";
 import { inject } from "@angular/core";
+import { CommonModule,NgIf } from '@angular/common';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent, CommonModule],
 })
 export class AppComponent {
   title = "ALTEN SHOP";
-  private cartService = inject(CartService);
+  cartService = inject(CartService);
+  count$ = this.cartService.count$;
 
-  cartCount = this.cartService.items$.pipe(
-    map(items => items.length)
-  );
 }
--- front/src/app/cart/data-access/cart.service.ts
+++ front/src/app/cart/data-access/cart.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Product } from 'app/products/data-access/product.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({ 
     providedIn: "root"
 }) export class CartService {
   private itemsSubject = new BehaviorSubject<Product[]>([]);
   items$: Observable<Product[]> = this.itemsSubject.asObservable();
+
+  readonly count$: Observable<number> = this.items$.pipe(
+    map(items => items.length)
+  );
 
   addToCart(product: Product): void {
     const current = this.itemsSubject.value;
